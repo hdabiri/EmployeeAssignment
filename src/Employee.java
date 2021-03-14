e257bfd043708cf889915c31a4fa22681ab643cd
Hidayah Dabiri <monisoladab@yahoo.com>
2021-03-13 20:53:37 -0500

    Completed Assignments

--- src/Employee.java
+++ src/Employee.java
@@ -3,11 +3,18 @@ public class Employee {
 	private int employeeId;
 	private String employeeLastName;
 	private String employeeFirstName;
+	
 	private double salary;
+	
 	private String workHours;
+	
 	private boolean isFullTime;
+	
 	private boolean isActive;
+	
 	private Role role;
+	
+	@SuppressWarnings("unused")
 	private Gender gender;
 	
 	
@@ -25,22 +32,69 @@ public class Employee {
 		this.salary = salary;
 		this.workHours = workHours;
 		this.isFullTime = isFullTime;
+		this.isActive = isActive;
 		this.role = role;
 		this.gender = gender;
-		this.isActive = isActive;
+		
 	}
-
+	
+	//Getting the first and last name of the employee
 	String getFullName() {
 		return this.employeeFirstName + " " + this.employeeLastName;
 				
 	}
 
+	//Printing the employee information
 	@Override
 	public String toString() {
-		return "EmployeeDemo [employeeId=" + employeeId + ", Name=" + this.getFullName() + ", role=" + role + "]";
+		return "Employee with ID number: " + employeeId +", named "+ 
+				this.getFullName() + ", in the role of " + role;
 	}
 
+	//To Fire an employee
+	void fireEmployee() {
+		this.isActive = false;
 
+	}
+	
+	//To Promote an employee
+	public Role promoteEmployee() {		
+		this.role = role.next();
+		return role;
+	}
+	
+	//To get the work status, full time or otherwise
+	public boolean getIsFullTime() {
+		return isFullTime;
+	}
+	
+	//To get the employment status of the employee
+	public boolean getIsActive() {
+		return isActive;
+	}
 
+	//To get the work hours of the employee
+	public String getWorkHours() {
+		return workHours;
+	}
 
+	//To change the work hours of the employee
+	public void setWorkHours(String workHours) {
+		this.workHours = workHours;
+	}
+
+	//To get the role of the employee
+	public Role getRole() {
+		return role;
+	}
+
+	//To get the salary of the employee
+	public double getSalary() {
+		return this.salary; 
+	}
+	
+	//To get the gender of the employee. I don't know what for?? :!
+	public Gender getGender() {
+		return this.gender;
+	}
 }
